a000a1b07d0b3d7f5ab94bfaa4a286cfc42f6f8e
Frederick <frederickfd88@gmail.com>
2020-08-21 08:50:21 +0700

    update on interactive vue-color interface

--- pages/vuecolor.vue
+++ pages/vuecolor.vue
@@ -5,13 +5,29 @@
       <div class="row">
         <div class="col-6">
           <h5>Chrome Picker</h5>
-          <Chrome v-model="colors3" />
-          <h5 class="text-secondary mt-3">ChromeData : {{ colors3 }}</h5>
+          <client-only>
+            <Chrome v-model="colors3" />
+          </client-only>
+          <h5 class="text-secondary mt-3">
+            ChromeData : take a look on console
+          </h5>
+          <div
+            class="vue-color-bg"
+            :style="{ backgroundColor: bgColor1 }"
+          ></div>
         </div>
         <div class="col-6">
           <h5>Sketch Picker</h5>
-          <Sketch v-model="colors4" />
-          <h5 class="text-secondary mt-3">SketchData : {{ colors4 }}</h5>
+          <client-only>
+            <Sketch v-model="colors4" />
+          </client-only>
+          <h5 class="text-secondary mt-3">
+            SketchData : take a look on console
+          </h5>
+          <div
+            class="vue-color-bg"
+            :style="{ backgroundColor: bgColor2 }"
+          ></div>
         </div>
       </div>
     </div>
@@ -25,13 +41,43 @@ export default {
     Chrome,
     Sketch
   },
+  watch: {
+    colors3() {
+      console.log(`Chrome : ${this.colors3}`);
+    },
+    colors4() {
+      console.log(`SketchData : ${this.colors4}`);
+    }
+  },
   data() {
     return {
       colors3: [],
       colors4: []
     };
+  },
+  computed: {
+    bgColor1() {
+      if (this.colors3.length === 0) {
+        return "black";
+      } else {
+        return this.colors3.hex;
+      }
+    },
+    bgColor2() {
+      if (this.colors4.length === 0) {
+        return "black";
+      } else {
+        return this.colors4.hex;
+      }
+    }
   }
 };
 </script>
 
-<style></style>
+<style>
+.vue-color-bg {
+  width: 60px;
+  height: 60px;
+  border-radius: 4px;
+}
+</style>
