05de0673890a8ee2ee0c440fd47179119106b031
Frederick <frederickfd88@gmail.com>
2020-10-30 17:39:07 +0700

    trying google drive file upload in nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -91,6 +91,7 @@ export default {
         "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
     },
     { src: "https://code.jquery.com/jquery-3.5.1.slim.min.js" },
+    { src: "https://apis.google.com/js/platform.js" },
     {
       src:
         "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
---
+++ pages/google-drive.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Google Drive File Upload</h3>
+    <div
+      class="g-savetodrive"
+      data-src="//example.com/path/to/myfile.pdf"
+      data-filename="My Statement.pdf"
+      data-sitename="My Company Name"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+<style></style>
