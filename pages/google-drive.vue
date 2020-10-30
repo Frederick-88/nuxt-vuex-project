62e48e68adfe069c16025c9526ce63b63916f5fe
Frederick <frederickfd88@gmail.com>
2020-10-30 18:08:37 +0700

    Done integrate file upload google drive

--- pages/google-drive.vue
+++ pages/google-drive.vue
@@ -3,16 +3,27 @@
     <h3>Google Drive File Upload</h3>
     <div
       class="g-savetodrive"
-      data-src="//example.com/path/to/myfile.pdf"
-      data-filename="My Statement.pdf"
-      data-sitename="My Company Name"
+      data-src="https://s3.us-east-2.amazonaws.com/staging-assets.offeo.com/project-exports/UhH0tpEuQH/videos/OFFEO-1603941580.mp4"
+      data-filename="YOUR-OFFEO-VIDEO"
+      data-sitename="OFFEO"
     ></div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  head() {
+    return {
+      script: [
+        {
+          src: "https://apis.google.com/js/platform.js",
+          async: true,
+          defer: true
+        }
+      ]
+    };
+  }
+};
 </script>
-<script src="https://apis.google.com/js/platform.js" async defer></script>
 
 <style></style>
