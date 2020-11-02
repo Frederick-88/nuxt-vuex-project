ffdb1835f6af3da518ec8043f38ecd3560777762
Frederick <frederickfd88@gmail.com>
2020-11-02 14:56:01 +0700

    create dropbx page,start integrate save-to-dropbox

---
+++ pages/dropbox.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>Google Drive File Upload</h3>
+    <button class="btn btn-primary">Save To Dropbox</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
